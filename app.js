447d853866e720c17f4220b43264a08778dd7507
Doug Wright <mrdougwright@gmail.com>
2015-05-25 09:27:52 -0700

    Added Procfile for heroku & update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 4020))
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', function(req, res){
 	res.send('Hello World!')
 })
 
-var server = app.listen(4020, function(){
-	var host = server.address().address
-	var port = server.address().port
-
-	console.log('Example app listening at http://%s:%s', host, port)
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port', app.get('port'))	
 })
