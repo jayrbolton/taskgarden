33b36f0576644e286906600595fe952d35474044
Jay R Bolton <jay@commitchange.com>
2015-07-23 15:53:28 -0700

    set up directory structure, install ejs, and add index.html to render on /index

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express')
 var app = express()
 
+app.set('views', __dirname + '/client/markup')
+
+app.engine('html', require('ejs').renderFile)
+
 app.get('/', function(req, res){
-	res.send('Hello World!')
+	res.render('index.html')
 })
 
-var server = app.listen(4020, function(){
+var server = app.listen(4200, function(){
 	var host = server.address().address
 	var port = server.address().port
 
-	console.log('Example app listening at http://%s:%s', host, port)
+	console.log('taskgarden listening at http://%s:%s', host, port)
 })
